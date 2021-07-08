6948ecf4592a2d1e92ae2aa784ad3b62fca028fe
wyl3waK <82113302+wyl3waK@users.noreply.github.com>
2021-07-08 16:11:34 -0700

    Add files via upload

---
+++ Codeforces/Maximum Distance/max.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	int x = 0;
+	int y = 0;
+    int arrayx[5000];
+    int arrayy[5000];
+    while(x != n) {
+        cin >> arrayx[x];
+		x += 1;
+	}
+    while(y != n) {
+        cin >> arrayy[y];
+		y += 1;
+	}
+
+	int max = 0;
+    int distance;
+    int xmen;
+    int ymen;
+	for(int i = 0; i < n - 1; i++) {
+        for (int j = i+1; j < n; j++) {
+            xmen = abs(arrayx[i] - arrayx[j]);
+            ymen = abs(arrayy[i] - arrayy[j]);
+
+            distance = xmen * xmen + ymen * ymen;
+
+		    if (distance > max) {
+                max = distance;
+            }
+
+        }
+
+	}
+    cout << max;
+
+	return 0;
+}
